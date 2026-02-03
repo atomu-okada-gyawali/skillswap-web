b7e714ba51c21532a7f4b0a6ce7b3068c0899a46
atomu okada gyawali <atomgyawali2@gmail.com>
2026-02-03 22:04:05 +0545

    seventh commit

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,23 +1,28 @@
+import { AuthProvider } from "@/context/AuthContext";
 import Header from "./_components/Header";
 import Sidebar from "./_components/Sidebar";
+import { ToastContainer } from "react-toastify";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-    return (
-        <div className='flex w-full min-h-screen'>
-            <div className='page-wrapper flex w-full'>
-                {/* Header/sidebar */}
-                <div className='xl:block hidden'>
-                    <Sidebar />
-                </div>
-                <div className='w-full bg-background'>
-                    {/* Top Header  */}
-                    <Header />
-                    {/* Body Content  */}
-                    <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 p-2">
-                        {children}
-                    </main>
-                </div>
-            </div>
+  return (
+    <div className="flex w-full min-h-screen">
+      <div className="page-wrapper flex w-full">
+        {/* Header/sidebar */}
+        <div className="xl:block hidden">
+          <Sidebar />
         </div>
-    );
+        <div className="w-full bg-background">
+          {/* Top Header  */}
+          <Header />
+          {/* Body Content  */}
+          <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8 p-2">
+            <AuthProvider>
+              {children}
+              <ToastContainer position="top-right" autoClose={3000} />
+            </AuthProvider>
+          </main>
+        </div>
+      </div>
+    </div>
+  );
 }
